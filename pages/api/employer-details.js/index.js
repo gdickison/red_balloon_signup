672365ce3afc2c788c2a6cc0bf3c64789f684a3d
paunchymonk <gregory@dickison.net>
2022-03-22 10:41:43 -0700

    Hook the new employer account inputs to an api file

--- pages/api/employer-details.js/index.js
+++ pages/api/employer-details.js/index.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Employer Details' })
+  const body = req.body
+  console.log('body', body)
+
+  res.status(200).json({ data: `${body.firstName} ${body.lastName}` })
 }
--- src/components/NewEmployerAccountDetail/NewEmployerAccountDetail.js
+++ src/components/NewEmployerAccountDetail/NewEmployerAccountDetail.js
@@ -2,14 +2,35 @@
 import SectionWrapper from "../SectionWrapper"
 import EmployerOnboardHeader from "../EmployerOnboardHeader"
 import ButtonWithArrow from "../../assets/ButtonWithArrow"
+import { useState } from 'react'
 
 const NewEmployerAccountDetail = () => {
+  const [newEmployerDetail, setNewEmployerDetail] = useState({})
   const changeHandler = e => {
-    console.log(e.target.value)
+    e.preventDefault()
+    setNewEmployerDetail({...newEmployerDetail, [e.target.name]: e.target.value})
+    console.log(newEmployerDetail)
   }
 
-  const eventHandler = () => {
+  const eventHandler = async e => {
+    e.preventDefault()
     console.log('click')
+    const JSONdata = JSON.stringify(newEmployerDetail)
+    const endpoint = '/api/employer-details'
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSONdata,
+    }
+
+    const response = await fetch(endpoint, options)
+
+    const result = await response.json()
+
+    console.log(result)
   }
   return (
     <SectionWrapper show={true}>
@@ -18,16 +39,14 @@ const NewEmployerAccountDetail = () => {
           headerText="Keep Going!"
           stepText="STEP 3/5 - We need just a bit more information to set up your account"
         />
-        <div className="mt-6 -mb-4">
           <img src="/images/great-seal-outlined.png" alt="Great Seal" className="drop-shadow-[0_0.5rem_0.5rem_rgba(0,0,0,0.50)]"/>
-        </div>
-         <div className="flex flex-col items-center space-y-3 my-12 relative">
-          <input id="first-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="first-name" placeholder="First Name" onChange={changeHandler}/>
-          <input id="last-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="last-name" placeholder="Last Name" onChange={changeHandler}/>
-          <input id="business-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="business-name" placeholder="Business Name" onChange={changeHandler}/>
+         <div className="flex flex-col items-center space-y-3 my-12">
+          <input id="first-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="firstName" placeholder="First Name" onChange={changeHandler}/>
+          <input id="last-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="lastName" placeholder="Last Name" onChange={changeHandler}/>
+          <input id="business-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="businessName" placeholder="Business Name" onChange={changeHandler}/>
           <input id="website" className="h-11 text-lg px-6 w-80 rounded-full" type="url" name="website" placeholder="Website" onChange={changeHandler}/>
-          <input id="address-1" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address-1" placeholder="Address 1" onChange={changeHandler}/>
-          <input id="address-2" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address-2" placeholder="Address 2" onChange={changeHandler}/>
+          <input id="address-1" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address1" placeholder="Address 1" onChange={changeHandler}/>
+          <input id="address-2" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address2" placeholder="Address 2" onChange={changeHandler}/>
           <input id="city" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="city" placeholder="City" onChange={changeHandler}/>
           <div className="flex w-80 justify-between">
             <input id="state" className="h-11 text-lg px-6 w-[9.5rem] rounded-full" type="text" name="state" placeholder="State" onChange={changeHandler}/>
