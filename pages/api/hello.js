7918d2acea1c714bff2900fea1986102fc4c40b6
paunchymonk <gregory@dickison.net>
2022-03-23 15:57:04 -0700

    Comment out some potentially offending code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,18 +2,18 @@
 import { Client } from 'pg'
 
 export default async function handler(req, res) {
-  const newTableQuery = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  CREATE TABLE employer_login (
-    id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    email varchar(40) NOT NULL,
-    hash_pass varchar(40) NOT NULL,
-    terms_accepted BOOLEAN NOT NULL
-  );`
-  const client = new Client({connectionString: process.env.DATABASE_URL})
+  // const newTableQuery = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  // CREATE TABLE employer_login (
+  //   id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  //   email varchar(40) NOT NULL,
+  //   hash_pass varchar(40) NOT NULL,
+  //   terms_accepted BOOLEAN NOT NULL
+  // );`
+  // const client = new Client({connectionString: process.env.DATABASE_URL})
 
-  await client.connect()
-  const response = await client.query(newTableQuery)
-  console.log(response)
-  await client.end()
+  // await client.connect()
+  // const response = await client.query(newTableQuery)
+  // console.log(response)
+  // await client.end()
   res.status(200).json({ name: 'John Doe' })
 }
