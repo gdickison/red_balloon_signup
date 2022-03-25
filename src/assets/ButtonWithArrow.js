2afad9a664cd52093ef89ddb5af8013b877e727b
paunchymonk <gregory@dickison.net>
2022-03-24 17:04:39 -0700

    Fix the uncle andrew image, tweak some styles

--- src/assets/ButtonWithArrow.js
+++ src/assets/ButtonWithArrow.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const ButtonWithArrow = ({buttonText, eventHandler}) => {
   return (
-    <button className="flex justify-between items-center text-rbWhite bg-rbBlue border-1 border-rbWhite h-11 w-80 border-2 rounded-full" onClick={eventHandler}>
+    <button className="flex justify-between items-center text-rbWhite bg-rbBlue border-1 border-rbWhite h-11 w-full border-2 rounded-full" onClick={eventHandler}>
       <span className="uppercase font-bold tracking-wider text-lg px-6">{buttonText}</span>
       <span className="flex items-center p-1.5">
         <Image className="bg-rbWhite rounded-full" width={28} height={28} src="/images/arrow-circle-right.svg" alt={buttonText} />
--- src/components/NewEmployerAccountDetail/NewEmployerAccountDetail.js
+++ src/components/NewEmployerAccountDetail/NewEmployerAccountDetail.js
@@ -42,12 +42,11 @@ const NewEmployerAccountDetail = () => {
           headerText="Keep Going!"
           stepText="STEP 3/5 - We need just a bit more information to set up your account"
         />
-        <div id="uncle-andrew" className="relative left-[110%] mt-8 xs:mt-0 flex flex-col justify-center items-center">
+        <div id="uncle-andrew" className="relative left-[150%] mt-8 xs:mt-0 flex flex-col justify-center items-center">
           <picture>
-            <source srcSet="/images/great-seal-outlined-sm.png" media="(max-width: 475px)"/>
-            <img src="/images/great-seal-outlined.png" alt="Great Seal" className="object-none h-[50vh] xs:-ml-3 drop-shadow-[0_0.5rem_0.5rem_rgba(0,0,0,0.50)]"/>
+            <source srcSet="/images/happy-rb-man-sm.png" media="(max-width: 475px)"/>
+            <img className="min-w-[500px] min-h-[400px]" src="/images/happy-rb-man.png" alt="Uncle Sam" />
           </picture>
-          <img className="absolute top-0 w-72 right-1/3" src="/images/uncle-sam.png" alt="Uncle Sam" />
           <div className="hero-quote-wrapper absolute top-56 w-64 flex z-10">
             <HeroQuote
               quote="&quot;We want you to succeed in business&#x21;&quot;"
@@ -61,18 +60,20 @@ const NewEmployerAccountDetail = () => {
             </div>
           </div>
         </div>
-        <div id="inputs" className="absolute top-[75%] flex flex-col items-center space-y-3">
-          <input id="first-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="firstName" placeholder="First Name" onChange={changeHandler}/>
-          <input id="last-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="lastName" placeholder="Last Name" onChange={changeHandler}/>
-          <input id="business-name" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="businessName" placeholder="Business Name" onChange={changeHandler}/>
-          <input id="website" className="h-11 text-lg px-6 w-80 rounded-full" type="url" name="website" placeholder="Website" onChange={changeHandler}/>
-          <input id="address-1" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address1" placeholder="Address 1" onChange={changeHandler}/>
-          <input id="address-2" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="address2" placeholder="Address 2" onChange={changeHandler}/>
-          <input id="city" className="h-11 text-lg px-6 w-80 rounded-full" type="text" name="city" placeholder="City" onChange={changeHandler}/>
-          <div className="flex w-80 justify-between">
-            <input id="state" className="h-11 text-lg px-6 w-[9.5rem] rounded-full" type="text" name="state" placeholder="State" onChange={changeHandler}/>
-            <input id="zip" className="h-11 text-lg px-6 w-[9.5rem] rounded-full" type="text" name="zip" placeholder="Zip" onChange={changeHandler}/>
+        <div id="inputs" className="absolute top-[75%] flex flex-col items-center space-y-3 mt-4 w-5/6 max-w-screen-sm">
+          <input id="first-name" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="firstName" placeholder="First Name" onChange={changeHandler}/>
+          <input id="last-name" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="lastName" placeholder="Last Name" onChange={changeHandler}/>
+          <input id="business-name" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="businessName" placeholder="Business Name" onChange={changeHandler}/>
+          <input id="website" className="h-11 text-lg px-6 w-full rounded-full" type="url" name="website" placeholder="Website" onChange={changeHandler}/>
+          <input id="address-1" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="address1" placeholder="Address 1" onChange={changeHandler}/>
+          <input id="address-2" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="address2" placeholder="Address 2" onChange={changeHandler}/>
+          <input id="city" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="city" placeholder="City" onChange={changeHandler}/>
+          <div className="flex w-full justify-between">
+            <input id="state" className="h-11 text-lg px-6 w-[48%] rounded-full" type="text" name="state" placeholder="State" onChange={changeHandler}/>
+            <input id="zip" className="h-11 text-lg px-6 w-[48%] rounded-full" type="text" name="zip" placeholder="Zip" onChange={changeHandler}/>
           </div>
+          <input id="why" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="why" placeholder="Why do you want to join RedBalloon?" onChange={changeHandler}/>
+          <input id="source" className="h-11 text-lg px-6 w-full rounded-full" type="text" name="source" placeholder="How did you hear about us?" onChange={changeHandler}/>
           <ButtonWithArrow
             buttonText="CONTINUE"
             eventHandler={eventHandler}
