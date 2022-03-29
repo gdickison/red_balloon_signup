961aae13a2421ebeeeffbe3910b400ca371ed6cf
paunchymonk <gregory@dickison.net>
2022-03-29 14:03:14 -0700

    Fix background too short on NewEmployerAccountDetail view

--- src/assets/Copyright.js
+++ src/assets/Copyright.js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router"
+
 const Copyright = () => {
+  const router = useRouter()
+
   return (
-    <div className="text-rbWhite text-xs mb-4 absolute bottom-0">
+    <div id="copyright" className={`text-rbWhite text-xs mb-4 absolute bottom-0 ${router.pathname === "/new-employer-account-detail" ? "opacity-0" : "opacity-100"}`}>
       <span>&copy; RedBalloon, All Rights Reserved</span>
     </div>
   )
--- src/components/SectionWrapper/SectionWrapper.js
+++ src/components/SectionWrapper/SectionWrapper.js
@@ -1,12 +1,9 @@
-import { useRouter } from "next/router"
-
 const SectionWrapper = ({children, show, id}) => {
-  const router = useRouter()
 
   return (
     <>
       {show &&
-        <section id={id} className={`relative flex flex-col items-center h-screen ${router.pathname === "/new-employer-account-detail" ? "min-h-[900px]" : "min-h-[900px]"}`} style={{backgroundImage: "url(/images/bg-red-paper.jpg)", backgroundSize: 'cover'}}>
+        <section id={id} className="relative flex flex-col items-center h-screen overflow-hidden min-h-[900px]" style={{backgroundImage: "url(/images/bg-red-paper.jpg)", backgroundSize: 'cover'}}>
           {children}
         </section>
       }
