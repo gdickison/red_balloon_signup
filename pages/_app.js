5faf9f06a4539b6b10b7527cbee8bb348b3d6ac5
Jonathan Crapuchettes <jcrapuchettes@gmail.com>
2022-04-21 14:54:16 -0700

    Add plausible based on documentation from https://github.com/4lejandrito/next-plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain='redballoon.work' customDomain='https://analytics.redballoon.work' selfHosted='true'>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
