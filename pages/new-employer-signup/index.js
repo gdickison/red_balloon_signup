47de305b222c5c01f6ac241e0abbe58932021b58
paunchymonk <gregory@dickison.net>
2022-04-22 09:50:50 -0700

    Hide iframe login page, and show iframe when it gets to purchase packages page

--- pages/new-employer-signup/index.js
+++ pages/new-employer-signup/index.js
@@ -11,13 +11,31 @@ const NewEmployerSignup = () => {
     }
   }
 
+  // For the parent frame to receive the message from the purchase complete page
+  window.addEventListener('message', (event) => {
+    if (event.origin !== 'https://www.redballoon.work')
+        return;
+    if (event.data == 'Purchase Done')
+    {
+        // Disable account
+        // Continue to next page
+    }
+    else if (event.data == 'On Purchase Page')
+    {
+        // Show iframe
+        document.getElementById('rb').style.display = 'block';
+    }
+    else
+        console.log(event);
+  });
+
   return (
     <div className="h-screen">
       <Head>
         <title>RedBalloon</title>
         <link rel="icon" type="image/png" href="/images/RedBalloon-Icon.png"/>
       </Head>
-      <iframe id="rb" src="https://www.redballoon.work/employer/purchase" title="RedBalloon main site" height="100%" width="100%" onLoad={runLogin}></iframe>
+      <iframe id="rb" src="https://www.redballoon.work/employer/purchase" title="RedBalloon main site" height="100%" width="100%" onLoad={runLogin} style={{ display: 'none'}}></iframe>
     </div>
   )
 }
