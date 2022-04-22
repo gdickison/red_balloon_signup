02010af1842fee7b9a226064ea6fa1e352ffe910
paunchymonk <gregory@dickison.net>
2022-04-22 15:34:02 -0700

    Try making it important

--- pages/new-employer-signup/index.js
+++ pages/new-employer-signup/index.js
@@ -6,26 +6,31 @@ const NewEmployerSignup = () => {
 
   const runLogin = () => {
     if(localStorage.getItem("email") && localStorage.getItem("password")){
+      console.log('start runLogin')
       const t = {'action':'login','username': localStorage.getItem("email"), 'password': localStorage.getItem("password")};
       document.getElementById('rb').contentWindow.postMessage(t, 'https://www.redballoon.work/login/employer/purchase');
       localStorage.clear()
+      console.log('end runLogin')
     }
   }
 
   useEffect(() => {
     // if (typeof window !== undefined) {
       window.addEventListener('message', (event) => {
+        console.log('start addEventListener')
         if (event.origin !== 'https://www.redballoon.work')
             return;
         if (event.data == 'Purchase Done')
         {
             // Disable account
             // Continue to next page
+            console.log('purchase done')
         }
         else if (event.data == 'On Purchase Page')
         {
             // Show iframe
-            document.getElementById('rb').style.display = 'block';
+            document.getElementById('rb').style.display = "block !important";
+            console.log('on purchase page')
         }
         else
             console.log(event);
@@ -39,7 +44,7 @@ const NewEmployerSignup = () => {
         <title>RedBalloon</title>
         <link rel="icon" type="image/png" href="/images/RedBalloon-Icon.png"/>
       </Head>
-      <iframe id="rb" src="https://www.redballoon.work/employer/purchase" title="RedBalloon main site" height="100%" width="100%" onLoad={runLogin} style={{ display: 'none'}}></iframe>
+      <iframe id="rb" src="https://www.redballoon.work/employer/purchase" title="RedBalloon main site" height="100%" width="100%" onLoad={runLogin} style={{ display: "none !important"}}></iframe>
     </div>
   )
 }
