6666637dbcb8dffab985816d4d622b8e841989f1
paunchymonk <gregory@dickison.net>
2022-04-26 09:45:57 -0700

    Make content cover device width in landscape mode - it ignores the safe area and the notch

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import PlausibleProvider from 'next-plausible'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <PlausibleProvider domain='redballoon.work' customDomain='https://analytics.redballoon.work' selfHosted='true'>
+      <Head>
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, viewport-fit=cover"/>
+      </Head>
       <Component {...pageProps} />
     </PlausibleProvider>
   )
